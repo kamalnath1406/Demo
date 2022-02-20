1f07fe3d05db3c47c045cab8bbc60dc98fbfe160
kamalnath1406 <kamalnath14061995@gmail.com>
2022-02-20 09:57:25 +0530

    My Task

---
+++ src/test/java/org/maven/GitHubSolutions.java
@@ -0,0 +1,19 @@
+package org.maven;
+
+public class GitHubSolutions {
+
+	
+	public static void main(String[] args) {
+
+		String str="Automation="+123 +"Engineer="+456;
+		String str1="Automation+Engineer="+(123+456);
+	
+	
+		System.out.println(str1);
+		
+	}
+
+}
+
+	
+
